83251a27530d5009959bfd3fb1ec7b7d1c7fe1d3
Saiful123ss <saifulammarzarifb@gmail.com>
2025-10-23 09:12:06 +0800

    follow template

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,25 +9,34 @@
   <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
 </head>
+
 <body>
   <div class="container-scroller">
 
-    {{-- Navbar atas (DILUAR page-body-wrapper) --}}
+    {{-- ✅ Top Navbar --}}
     @include('partials.navbar')
 
     <div class="container-fluid page-body-wrapper">
 
-      {{-- Sidebar kiri --}}
+      {{-- ✅ Sidebar (fixed left) --}}
       @include('partials.sidebar')
 
-      {{-- Main content kanan --}}
+      {{-- ✅ Main Content Area --}}
       <div class="main-panel">
         <div class="content-wrapper">
           @yield('content')
         </div>
-      </div>
 
+        {{-- Optional Footer --}}
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">© {{ date('Y') }} Purple Dashboard</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Made with ❤️ by <a href="#" target="_blank">You</a></span>
+          </div>
+        </footer>
+      </div>
     </div>
   </div>
 
